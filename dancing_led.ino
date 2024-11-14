fc1e15786862531d36349e3d27444327e4e594e0
soundarya m p <122505807+soundaryamp09@users.noreply.github.com>
2024-11-14 19:29:46 +0530

    Add files via upload

---
+++ dancing_led.ino
@@ -0,0 +1,88 @@
+int Led_1=1;
+int Led_2=2;
+int Led_3=3;
+int Led_4=4;
+int Led_5=5;
+int Led_6=6;
+int Led_7=7;
+int Led_8=8;
+int Led_9=9;
+int Led_10=10;
+int Led_11=11;
+int Led_12=12;
+int Led_13=13;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(Led_1,OUTPUT);
+pinMode(Led_2,OUTPUT);
+pinMode(Led_3,OUTPUT);
+pinMode(Led_4,OUTPUT);
+pinMode(Led_5,OUTPUT);
+pinMode(Led_6,OUTPUT);
+pinMode(Led_7,OUTPUT);
+pinMode(Led_8,OUTPUT);
+pinMode(Led_9,OUTPUT);
+pinMode(Led_10,OUTPUT);
+pinMode(Led_11,OUTPUT);
+pinMode(Led_12,OUTPUT);
+pinMode(Led_13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(Led_1 ,HIGH);
+delay(100);
+digitalWrite(Led_2 ,HIGH);
+delay(100);
+digitalWrite(Led_3 ,HIGH);
+delay(100);
+digitalWrite(Led_4 ,HIGH);
+delay(100);
+digitalWrite(Led_5 ,HIGH);
+delay(100);
+digitalWrite(Led_6 ,HIGH);
+delay(100);
+digitalWrite(Led_7 ,HIGH);
+delay(100);
+digitalWrite(Led_8 ,HIGH);
+delay(100);
+digitalWrite(Led_9 ,HIGH);
+delay(100);
+digitalWrite(Led_10 ,HIGH);
+delay(100);
+digitalWrite(Led_11 ,HIGH);
+delay(100);
+digitalWrite(Led_12 ,HIGH);
+delay(100);
+digitalWrite(Led_13 ,HIGH);
+delay(100);
+
+digitalWrite(Led_1 ,LOW);
+delay(100);
+digitalWrite(Led_2 ,LOW);
+delay(100);
+digitalWrite(Led_3 ,LOW);
+delay(100);
+digitalWrite(Led_4 ,LOW);
+delay(100);
+digitalWrite(Led_5 ,LOW);
+delay(100);
+digitalWrite(Led_6 ,LOW);
+delay(100);
+digitalWrite(Led_7 ,LOW);
+delay(1000);
+digitalWrite(Led_8 ,LOW);
+delay(100);
+digitalWrite(Led_9,LOW);
+delay(100);
+digitalWrite(Led_10 ,LOW);
+delay(100);
+digitalWrite(Led_11 ,LOW);
+delay(100);
+digitalWrite(Led_12 ,LOW);
+delay(100);
+digitalWrite(Led_13 ,LOW);
+delay(100);
+
+}
